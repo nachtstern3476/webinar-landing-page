9096f56299582cb0ccb20c2f93b0c9467dd410f6
nachtstern3476 <yeremiawijaya2@gmail.com>
2024-03-14 11:44:06 +0700

    update all landing page brand section

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: '/webinar-landing-page/',
+	base: "/webinar-landing-page/",
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, 'index.html'),
-				v2: resolve(__dirname, 'landing-page-v2.html'),
-				v3: resolve(__dirname, 'landing-page-v3.html'),
+				main: resolve(__dirname, "index.html"),
+				v2: resolve(__dirname, "landing-page-v2.html"),
+				v3: resolve(__dirname, "landing-page-v3.html"),
 			},
 		},
 	},
-	plugins: [ViteImageOptimizer()],
 });
